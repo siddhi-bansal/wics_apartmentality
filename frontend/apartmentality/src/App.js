24cc9f29982bb58a7208ac9875a7d2f3ee12936c
Pranjal Rai <pranjalr@cs.utexas.edu>
2024-03-01 11:31:57 -0600

    removed the mojo dojo casa house

--- frontend/apartmentality/src/App.js
+++ frontend/apartmentality/src/App.js
@@ -13,12 +13,7 @@ function App() {
     <div className="App">
       <div className="header">
         <div className="title">
-          <img src="./logo-final.png"className="logo"></img>
-          {/* <img src="./logo-lc.png"></img> */}
-          {/* <img src="./house-icon"></img> */}
-          {/* <h1>Apartmentality <img src='./logo192.png'></img></h1>
-          <p>Find Your ✨ New Mojo Dojo Dreamhouse ✨ Today!</p> */}
-          {/* <p>✨ Find Your New Mojo Dojo Dreamhouse Today! ✨</p> */}
+          <img src="./logo_actually.png"className="logo"></img>
         </div>
         </div>
       <Tabs activeTab={activeTab} setActiveTab={setActiveTab} />
